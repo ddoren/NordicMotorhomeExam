5a28560a0cbfc51b22821361610bf01297f6e6b2
Dorin <jtrettore@gmail.com>
2020-06-03 14:16:14 +0200

    jUnit Test

--- NordicMotorhomeExam/src/test/java/com/example/demo/DemoApplicationTests.java
+++ NordicMotorhomeExam/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import com.example.demo.Model.Reservation;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class ReservationTest {
+
+    //Testing The First Option, less than
     @Test
-    void contextLoads() {
+    void calculateCancelPriceTest1() {
+        Reservation test = new Reservation();
+        assertEquals(500, test.calculateCancelPrice(1000, 20));
     }
 
+    @Test
+    void calculateCancelPriceTest2() {
+        Reservation test = new Reservation();
+        assertEquals(1600, test.calculateCancelPrice(2000, 10));
+    }
+
+    @Test
+    void calculateCancelPriceTest3() {
+        Reservation test = new Reservation();
+        assertEquals(3000, test.calculateCancelPrice(3000, 55));
+    }
 }
